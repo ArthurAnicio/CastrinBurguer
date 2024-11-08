8485dae0fc647303f56239dc8ae6b42f78b54ed6
sad0ilem1 <andersonrunguefilho@gmail.com>
2024-11-08 07:37:30 -0300

    CommitServer

---
+++ server/src/db/migrations/00_create_produtos.ts
@@ -0,0 +1,17 @@
+import { Knex } from 'knex';
+
+export async function up(knex: Knex){
+    return (
+        knex.schema.createTable('produtos', (table) => {
+            table.increments('id').primary();
+            table.string('nome').notNullable();
+            table.string('descricao').notNullable().unique();
+            table.string('preco').notNullable();
+            table.integer('quantidade').notNullable();
+        })  
+    )
+}
+
+export async function down(knex: Knex){
+    return knex.schema.dropTable('produtos')
+}
