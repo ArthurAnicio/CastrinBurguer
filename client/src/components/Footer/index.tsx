2a147621d57f17b8c3efc7c0bc5fdbf87864f7e1
ArthurAnicio <arthuranicio06@gmail.com>
2024-11-08 08:59:30 -0300

    fixes

---
+++ client/src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+import './styles.css'
+
+function Footer(){
+    return(
+        <footer>
+            Opa
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ client/src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import './styles.css'
+
+function Header(){
+    return(
+        <header>
+            <img src="https://cdn.discordapp.com/attachments/1005168389955797095/1304414748963635210/CastrinBurguer.png?ex=672f4e67&is=672dfce7&hm=7a847a969883c56cc03e6ea078ccde59c662d9ad61eb3ab589fb84a1ae0f3711&" alt="Foto burguer" />
+        </header>
+    )
+}
+
+export default Header;
